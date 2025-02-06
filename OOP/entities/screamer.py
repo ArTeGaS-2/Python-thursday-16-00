0e5e659e84eba2c745e5fa842a7d19123d364832
ArTeGaS-2 <probablytorvik@gmail.com>
2025-02-06 19:55:10 +0200

    Додано і налаштовано скрімер при колізії

---
+++ OOP/entities/screamer.py
@@ -0,0 +1,37 @@
+import pygame
+
+class Screamer:
+    def __init__(self):
+        # Початкове значення: не активний
+        self.active = False
+
+        # Завантажуємо зображення
+        self.image = pygame.image.load("OOP/assets/screamer.png").convert_alpha()
+
+        # Завантажуємо звук
+        self.sound = pygame.mixer.Sound("OOP/assets/screamer.mp3")
+        # Регулюємо гучнічть від 0 до 1
+        self.sound.set_volume(0.1)
+
+    def resize(self, width, height):
+        """Мастабуємо зображення під розміри екрану."""
+        self.image = pygame.transform.scale(self.image, (width, height))
+
+    def activate(self):
+        """Активуємо скрімер: встановлюємо прапорець і запускаємо звук"""
+        if not self.active:
+            self.active = True
+            if self.sound:
+                self.sound.play(-1) # Зациклене відтворення
+
+    def deactivate(self):
+        """Деактивуємо скрімер: скидаємо прапорець і зупиняємо звук"""
+        if self.active:
+            self.active = False
+            if self.sound:
+                self.sound.stop()
+    
+    def draw(self, screen):
+        """Малюємо скрімер на екрані, якщо він активний"""
+        if self.active and self.image:
+            screen.blit(self.image, (0,0))
--- OOP/game_manager.py
+++ OOP/game_manager.py
@@ -9,6 +9,9 @@ from entities.game_object import GameObject
 from entities.vertical_enemy import VerticalEnemy
 from entities.patrol_enemy import PatrolEnemy
 
+# Скрімер
+from entities.screamer import Screamer
+
 class GameManager:
     def __init__(self):
         pygame.init()
@@ -21,6 +24,11 @@ class GameManager:
         # Створення спрайт-груп
         self.all_sprites = pygame.sprite.Group()
         self.collectibles = pygame.sprite.Group()
+
+        # Створюємо скрімер
+        self.screamer = Screamer()
+        self.screamer.resize(WIDTH, HEIGHT)
+
         # Група ворогів
         self.enemies = pygame.sprite.Group()
 
@@ -87,10 +95,24 @@ class GameManager:
             self.slime, self.collectibles, True)
         self.collected_objects += len(collided)
 
+        # Перевірка зіткнень з ворогами
+        collided_found = False
+        for enemy in self.enemies:
+            if pygame.sprite.collide_rect(self.slime, enemy):
+                enemy.handle_collision_with_player(self.slime)
+                collided_found = True
+
+        if collided_found:
+            self.screamer.activate()
+        else:
+            self.screamer.deactivate()
+
+
     def draw(self):
         #self.screen.fill(self.background_color)
         self.all_sprites.draw(self.screen)
         self.display_score()
+        self.screamer.draw(self.screen)
 
     def display_score(self):
         text = self.font.render(f'Зібрано: {self.collected_objects}',
